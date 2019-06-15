81104db9afe6ae4e20abd2c24eaf57221e68ee5b
PratikRajani <pratikrajani98@gmail.com>
2019-06-15 17:21:48 +0530

    added simple graph class

--- main.py
+++ main.py
@@ -31,6 +31,32 @@ class Graph:
         else:
             return False
 
+    def isCycleUtil(self, node, visited, parent):
+        visited[node] = True
+        print('p', node)
+        for adj in self.graph[node]:
+            if visited[adj] == False:
+                print('c', adj)
+                if self.isCycleUtil(adj, visited, node) == True:
+                    return True
+            elif parent != adj:
+                return True
+        return False
+
+    def isCycle(self):
+        visited = [False]*(self.nodes+1)
+        for node in range(self.nodes):
+            if visited[node+1] == False:
+                if self.isCycleUtil(node+1, visited, -1) == True:
+                    return True
+        return False
+
+    def isSimpleGraph(self):
+        if self.isCycle() == True:
+            return False
+        else:
+            return True
+
 #Driver code
 graph = Graph()
 
@@ -48,8 +74,13 @@ for i in range(0,edges):
     u,v = input('Enter edge nodes u and v: ').split(" ")
     u = int(u)
     v = int(v)
+    if u <= 0 or v <= 0 or u > nodes or v > nodes:
+        print("Invalid")
+        i = i-1
+        continue     
     graph.addEdge(u,v)
 
 print('isEdgeLessGraph : ' , graph.isEdgeLessGraph())
+print('isSimpleGraph : ' , graph.isSimpleGraph())
 
 print("---------DONE------------")
